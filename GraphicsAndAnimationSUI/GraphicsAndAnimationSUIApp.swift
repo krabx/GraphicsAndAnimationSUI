913b700858c01027a283d0bf12730d416195b6c5
Bogdan Radchenko <inroph@mail.ru>
2023-05-08 14:12:33 +0300

    Initial Commit

---
+++ GraphicsAndAnimationSUI/GraphicsAndAnimationSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicsAndAnimationSUIApp.swift
+//  GraphicsAndAnimationSUI
+//
+//  Created by Богдан Радченко on 08.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GraphicsAndAnimationSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
